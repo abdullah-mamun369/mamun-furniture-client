c0f80f5f1ede1c3d36ef5ae23a3958e703f30757
abdullah-mamun369 <maamamun369@gmail.com>
2022-11-25 23:54:17 +0600

    authcontext created

---
+++ src/Context/AuthProvider.js
@@ -0,0 +1,36 @@
+import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext();
+const auth = getAuth(app)
+
+
+const AuthProvider = ({ children }) => {
+
+
+    // SignUp with email and password
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    //Login with email and password
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+
+
+    const authInfo = {
+        createUser,
+        signIn
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
