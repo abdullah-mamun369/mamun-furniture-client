7c508da3b4c35835096f76d499a2678457c0f83b
abdullah-mamun369 <maamamun369@gmail.com>
2022-11-25 22:42:27 +0600

    Product added

--- src/Pages/Products/Product.js
+++ src/Pages/Products/Product.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Product = ({ product }) => {
 
-    const { category } = product
+    const { name, image, pickLocation, resalePrice, originalPrice, yearsOfUse, postDate, seller, verifySeller, condition } = product
+    console.log(verifySeller);
 
     return (
         <div>
-            <h1>{category}</h1>
-        </div>
+            <div className="card bg-base-200 shadow-xl p-5">
+                <div className='flex justify-between mb-2'>
+                    <h2 className="text-2xl font-semibold">{name}</h2>
+                    <h1 className='text-3xl font-bold text-primary'>{resalePrice}tk</h1>
+                </div>
+
+                <img src={image} alt="" className="rounded-xl" />
+
+                <p className='badge badge-primary text-white mt-3'>Condition: {condition}</p>
+
+                <div className='mt-3 flex'>
+                    <p className='text-xl font-semibold text-accent'>Seller: {seller}</p>
+                    {
+                        verifySeller ?
+                            <p> < BsPatchCheckFill color="cornflowerblue" /> </p> : <p> </p>
+                    }
+                </div>
+                <p className='text-sm text-slate-400'>Posted on {postDate}</p>
+
+                <div className="mt-3">
+                    <p><span className=' font-semibold'>Pick Up Point:</span> {pickLocation}</p>
+                    <p><span className=' font-semibold'>Original Price:</span> {originalPrice}tk</p>
+                    <p><span className=' font-semibold'>Years of use:</span> {yearsOfUse}</p>
+                </div>
+            </div>
+        </div >
     );
 };
 
 export default Product;
+
+
+// Private Route:  Category page will contain every item under that category. Product card should include 1. Pic 2. Name 3. Location/PickUpPoint 4. ResalePrice 5. OriginalPrice 6. YearsOfUse 7. PostTime 8. SellerName 9. VerifiedSeller <BookNowButton> 
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Product from './Product';
+import banner1 from '../../assets/banner1.png'
 
 const Products = () => {
 
     const products = useLoaderData();
-    console.log(products);
+    const categoryName = products[0].category;
 
     return (
-        <div className='mt-32 container mx-auto'>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 lg:mb-[130px] mb-20 mx-3'>
+        <div className='mt-24 container mx-auto'>
+            <div>
+                <img className='w-full' src={banner1} alt="" />
+            </div>
+            <h1 className='text-5xl text-accent font-bold text-center mt-12 mb-8'>{categoryName}</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-4 gap-6 lg:mb-[130px] mb-20 mx-3'>
                 {
                     products.map(product => <Product
                         key={product._id}
