292da6e6ffe7299073f4427893d9eb58cd60b590
abdullah-mamun369 <maamamun369@gmail.com>
2022-11-28 02:32:56 +0600

    Product sold status done

--- src/Pages/SellerDashboard/MyProductList.js
+++ src/Pages/SellerDashboard/MyProductList.js
@@ -7,6 +7,36 @@ const MyProductList = ({ product, i, myProducts }) => {
     const { _id, image, name, category, resalePrice, status } = product;
     // console.log("rnq checking", product);
 
+    // Delete Product==============================================================
+    const handleStatusUpdate = id => {
+        const proceed = window.confirm('Are you sure, the Product is sold?');
+        if (proceed) {
+            fetch(`http://localhost:7000/products/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json',
+                    // authorization: `Bearer ${localStorage.getItem('genius-token')}`
+                },
+                body: JSON.stringify({ status: 'Sold' })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.modifiedCount > 0) {
+                        const remaining = myProducts.filter(odr => odr._id !== id);
+                        const approving = myProducts.find(odr => odr._id === id);
+                        approving.status = 'Sold'
+
+                        const newProducts = [approving, ...remaining];
+                        window.location.reload(true);
+                    }
+                })
+        }
+
+    }
+
+
+    // Delete Product=========================================================
     const handleDelete = id => {
         const proceed = window.confirm('Are you sure, you want to cancel this order');
         if (proceed) {
@@ -45,10 +75,11 @@ const MyProductList = ({ product, i, myProducts }) => {
                 <div className="text-sm">{resalePrice}</div>
             </td>
             <td>
-                <button className="btn btn-ghost btn-xs">{status ? status : 'Available'}</button>
+                <button onClick={() => handleStatusUpdate(_id)} className="btn btn-ghost btn-xs">{status ? status : 'Available'}</button>
             </td>
             <td>
-                <button className="btn btn-primary btn-xs text-white">Advertise</button>
+
+                <button className="btn btn-primary btn-xs text-white" disabled={status && "disabled"}>Advertise</button>
             </td>
             <th>
                 <Link onClick={() => handleDelete(_id)} className=""><AiFillDelete color="red" fontSize="30px" /></Link>
